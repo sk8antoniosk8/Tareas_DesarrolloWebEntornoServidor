2a99a655278a3be0fc1b5f67f36c58d59c36ed50
sk8antoniosk8 <you@example.com>
2021-10-29 00:14:23 +0200

    Tareasphp

---
+++ adios.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Primera tarea PHP</title>
+  </head>
+  <body>
+    <?php echo "Hasta luego desarrollador!!!" ?>
+  </body>
+</html>
---
+++ hola.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Primer php</title>
+  </head>
+  <body>
+    <?php echo "Hola Paco campeon!!!!" ?>
+  </body>
+</html>
